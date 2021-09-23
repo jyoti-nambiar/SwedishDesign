a1e0ebe624bb9bdc3a65b5c7eed2d68782eac8eb
NoahGordon12 <noah.gordon@medieinstitutet.se>
2021-09-23 20:13:54 +0200

    andring

---
+++ wp-content/themes/groupProject/header.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "hejjjj";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Furniture Website</title>
+    <!-- LInk To CSS -->
+
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <header>
+        <a href="#" class="logo">Swedish <span>Designs</span></a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#new">New Arrival</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#brands">Our Partners</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </header>
